cbd7c9a59aff9ea31565266850ba448001f2258e
Emil Guseynov <theluckyemil@gmail.com>
2019-04-13 20:28:57 +0300

    game_id func

--- bls/main.cpp
+++ bls/main.cpp
@@ -1,6 +1,5 @@
 #include <sdk.h>
 #include <stdlib.h>
-
 #include <bls/bls.hpp>
 #include <string>
 
@@ -35,4 +34,60 @@ char *invoke(const char *str, int length) {
 
     return (char*)string.data();
 }
+
+static int game_id_autoinc = 0;
+
+
+struct pub_key_type {
+    char *data;
+    int len;
+};
+
+struct Game {
+    bool new_game(int n) {
+        len = 0;
+        players = (char**)malloc(sizeof(char*) * n);
+        stakes = (int*)malloc(sizeof(int) * n);
+        game_id = game_id_autoinc++;
+        return true;
+    }
+
+    bool add_stake(int stake, pub_key_type pub_key) {
+        stakes[len] = stake;
+        players[len] = pub_key.data;
+        ++len;
+    }
+
+    int get_stake(pub_key_type pub_key) {
+        for (int i = 0; i < len; i++) {
+            if (compare(players[i], pub_key.data, pub_key.len))
+                return stakes[i];
+        }
+        return 0;
+    }
+
+    bool compare(char *a, char *b, int len) {
+        for (int i = 0; i < len; i++) {
+            if (a[i] != b[i])
+                return false;
+        }
+        return true;
+    }
+
+
+    int get_game_id() {
+        return game_id;
+    }
+
+    char** get_participants() {
+        return players;
+    }
+private:
+    int *stakes;
+    char **players;
+    int len;
+    int game_id;
+};
+
+
 }
